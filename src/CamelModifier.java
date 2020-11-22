5bf264e742cd0cddc13675f799fb26ee189e6abd
rohitduvvuru <63662602+rohitduvvuru@users.noreply.github.com>
2020-11-21 23:20:15 -0800

    CamelModifier v.1.1

--- src/CamelModifier.java
+++ src/CamelModifier.java
@@ -1,2 +1,16 @@
 public class CamelModifier {
+    int hydration;
+    int stamina;
+
+    public void findOasis() {
+        System.out.print("You found an oasis");
+    }
+
+    public void sandstorm() {
+        System.out.println("You got caught in a sandstorm");
+    }
+    
+    public void cactusFlower() {
+        System.out.println("You found a special cactus flower");
+    }
 }
