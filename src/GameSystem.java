b8257b1a18f4fb2b2484fe378b5a123c3e813cd2
rohitduvvuru <63662602+rohitduvvuru@users.noreply.github.com>
2020-11-21 23:20:38 -0800

    GameSystem v.1.1

--- src/GameSystem.java
+++ src/GameSystem.java
@@ -1,5 +1,54 @@
+import java.util.Scanner;
+
 public class GameSystem {
     public static void main(String[] args) {
-        
+        boolean lose = false;
+        boolean win = false;
+        String state = "";
+        Camel camel = new Camel();
+        Scanner scan = new Scanner(System.in);
+
+        while (!win && !lose){
+
+            System.out.println("Enter run, walk, hydrate, rest, quit");
+            state = scan.nextLine();
+            if (state.equals("run")){
+                camel.run();
+                lose = camel.gameOverLose();
+                win = camel.gameOverWin();
+                System.out.println("Hydration: " + camel.getHydration() + "\n" 
+                		+ "Stamina: " + camel.getStamina() + "\n" 
+                		+ "Progress: " + camel.getProgess() + "\n" 
+                		+ "Enemy Progress: " + camel.getEnemy() + "\n");
+            }
+            else if (state.equals("walk")){
+                camel.walk();
+                lose = camel.gameOverLose();
+                win = camel.gameOverWin();
+                System.out.println("Hydration: " + camel.getHydration() + "\n" 
+                		+ "Stamina: " + camel.getStamina() + "\n" 
+                		+ "Progress: " + camel.getProgess() + "\n" 
+                		+ "Enemy Progress: " + camel.getEnemy() + "\n");
+            }
+            else if (state.equals("hydrate")){
+                camel.hydrate();
+                System.out.println("Hydration: " + camel.getHydration() + "\n" 
+                		+ "Stamina: " + camel.getStamina() + "\n" 
+                		+ "Progress: " + camel.getProgess() + "\n" 
+                		+ "Enemy Progress: " + camel.getEnemy() + "\n");
+            }
+            else if (state.equals("rest")){
+                camel.rest();
+                System.out.println("Hydration: " + camel.getHydration() + "\n" 
+                		+ "Stamina: " + camel.getStamina() + "\n" 
+                		+ "Progress: " + camel.getProgess() + "\n" 
+                		+ "Enemy Progress: " + camel.getEnemy() + "\n");
+            }
+            else if (state.equals("quit")) {
+                win = true;
+                lose = true;
+            }
+        }
+        scan.close();
     }
 }
