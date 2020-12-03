e2b6d16cd883d8c01d0036397081ebc96b39db49
rohitduvvuru <63662602+rohitduvvuru@users.noreply.github.com>
2020-12-02 22:51:12 -0800

    StatsView v.1.2
    Fixed the status bars; let me know if any changes are needed.

--- StatsView.java
+++ StatsView.java
@@ -14,8 +14,6 @@ public class StatsView extends JPanel {
 
         public StatsView(int n1, int n2, int n3, int n4){
             //initialize variables
-        	//setLayout(null);
-        	hydrationLabel.setLocation(100,100);
         	add(hydrationLabel);
             add(staminaLabel);
             add(progressLabel);
@@ -42,47 +40,48 @@ public class StatsView extends JPanel {
             Graphics2D g2 = (Graphics2D) g;
 
             //Stick values to bottom of JFrame
-            //int x1 = 10;
-            //int x2 = 10;
-            //int x3 = 10;
-            //int x4 = 10;
             int length = 120;
-            //add(hydrationLabel);
-            //hydrationLabel.setLocation(10,10);
+            int y = 120;
 
             //Rectangle for n1
 
             g2.setColor(Color.CYAN);
-            Rectangle rec1 = new Rectangle(27, 100 - (n1*10), 5, length - (100 - (n1*10)));
+            Rectangle rec1 = new Rectangle(27, 120 - (n1*10), 5, length - (120 - (n1*10)));
             g2.draw(rec1);
             g2.fill(rec1);
 
             //Rectangle for n2
 
             g2.setColor(Color.GREEN);
-            Rectangle rec2 = new Rectangle(40, 100 - (n2*10), 5, length - (100 - (n2*10)));
+            Rectangle rec2 = new Rectangle(40, 120 - (n2*10), 5, length - (120 - (n2*10)));
             g2.draw(rec2);
             g2.fill(rec2);
 
             //Rectangle for n3
+            
             g2.setColor(Color.BLUE);
-            Rectangle rec3 = new Rectangle(52, 100 - ((int) (n3*0.4)), 5, length - 100 - ((int) (n3*0.4)));
+            Rectangle rec3 = new Rectangle(52, y, 5, ((int) (n3*0.4)));
+            rec3.setLocation(52, y - ((int) (n3*0.4)));
             g2.draw(rec3);
             g2.fill(rec3);
 
             //Rectangle for n4
 
             g2.setColor(Color.RED);
-            Rectangle rec4 = new Rectangle(65, 100 - ((int) (n4*0.4)), 5, length - 100 - ((int) (n4*0.4)));
+            Rectangle rec4 = new Rectangle(65, y, 5, ((int) (n4*0.4)));
+            rec4.setLocation(65, y - ((int) (n4*0.4)));
             g2.draw(rec4);
             g2.fill(rec4);
             
-            //finish line (for reference)
+            //start and finish line (for reference)
             
             g2.setColor(Color.BLACK);
             Rectangle rec5 = new Rectangle(26, 120, 44, 1);
             g2.draw(rec5);
             g2.fill(rec5);
+            Rectangle rec6 = new Rectangle(26, 20, 44, 1);
+            g2.draw(rec6);
+            g2.fill(rec6);
 
         }
 }
